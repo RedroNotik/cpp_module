9ee789f4b8702699b861afd13ba39f0664b8d21f
RedroNotik <timofey150500@gmail.com>
2022-03-29 20:35:14 +0300

    Add ex00

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,44 @@
+//
+// Created by Gaynell Hanh on 3/29/22.
+//
+
+#ifndef CPP_MODULE_BUREAUCRAT_HPP
+#define CPP_MODULE_BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+protected:
+	Bureaucrat();
+private:
+	const std::string name;
+	int grade;
+public:
+	class GradeTooLowException:public std::exception
+	{
+		virtual const char* what() const throw(){
+			return ("Exception:Grade too low");
+		}
+	};
+	class GradeTooHighException:public std::exception
+	{
+		virtual const char* what() const throw(){
+			return ("Exception:Grade too high");
+		}
+	};
+	Bureaucrat(const std::string &name, int grade);
+	Bureaucrat(Bureaucrat const &rhs);
+	~Bureaucrat();
+	Bureaucrat &operator=(Bureaucrat const &rhs);
+
+	std::string getName() const;
+	int getGrade(void) const;
+	void IncrGrade();
+	void DecrGrade();
+
+};
+
+std::ostream & operator<<(std::ostream &o, Bureaucrat const & i);
+
+#endif //CPP_MODULE_BUREAUCRAT_HPP
